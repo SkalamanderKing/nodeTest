76c937f3f907edb2348670213063086466d82daf
SkalamanderKing <skala_mander@yahoo.se>
2017-05-03 17:13:28 +0200

    Some testning

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 let app = express();
 const port = 4000;
+
 app.get('/', (req, res) => {
-res.send('Hellooo');
+res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/about', (req, res) => {
-	res.send('Helloo from about')
-});
+// app.get('/about', (req, res) => {
+// 	res.sendfile(_dirname, 'index.html')
+// });
+
 
 app.listen(port, () =>{
 	console.log("Running noW!")
