12aefeaa69547a1036472c11f49929b366721acd
SkalamanderKing <skala_mander@yahoo.se>
2017-05-03 17:18:38 +0200

    pug and more

--- app.js
+++ app.js
@@ -2,13 +2,10 @@ const express = require('express');
 let app = express();
 const port = 4000;
 
-app.get('/', (req, res) => {
-res.sendFile(__dirname + '/index.html');
-});
+//app.get('/', (req, res) => {
+//res.sendFile(__dirname + '/index.html');
+//});
 
-// app.get('/about', (req, res) => {
-// 	res.sendfile(_dirname, 'index.html')
-// });
 
 
 app.listen(port, () =>{
